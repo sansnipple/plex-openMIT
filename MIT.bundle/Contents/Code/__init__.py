aadde1b5bc11f4bfdc18c19d6cbe4a42bd73f063
Billy Joe <sansnipple@gmail.com>
2009-08-02 07:44:07 -0400

    audio works 90%

--- MIT.bundle/Contents/Code/__init__.py
+++ MIT.bundle/Contents/Code/__init__.py
@@ -16,6 +16,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+# Known issues:
+#	because the audio is listed with the videos under the videos section in plex, it refuses to open the visualizer for it
+# only gets part of of 2 part mp3s, affects only one audio course.
+# one audio course i cant pin down, wont open "Symmetry, Structure, and Tensor Properties of Materials"
+# cant play rm audio, so it show just empty folder, only one occurance of this
+# Needs art
+
+
 import re, string
 from PMS import *
 from PMS.Objects import *
@@ -57,8 +65,9 @@ def video(sender):
 #		Log(url)
 		dir.Append(Function(DirectoryItem(getListing, title=title+""), title2=title+"", url=url+"", media="video"))
 
-	
 	return dir
+
+##################################
 
 def audio(sender):
 	dir = MediaContainer(title2="Audio Lectures")	
@@ -70,18 +79,15 @@ def audio(sender):
 	for x in range(len(aCourses)):
 		title = aCourses[x].text
 		url = "http://ocw.mit.edu" + frontPage.xpath("//img[@alt='Complete audio lectures']//parent::*//parent::*//parent::*//td[4]/a//@href")[x]
-#		Log(title)
-#		Log(url)
 		dir.Append(Function(DirectoryItem(getListing, title=title+""), title2=title+"", url=url+"", media="audio"))
-	
-
-	
-	
-	
 	return dir
+
+########################################################################
 
 def getListing(sender, title2, url, media):
 	dir = MediaContainer(title2=title2)
+	# these are a real bitch, there's like 3 differnt versions of each page, espisially audio, hence all the nested if statements
+	
 	# For Video Courses
 	if media == "video":
 		url = url.replace("CourseHome/index.htm", "VideoLectures/index.htm")
@@ -109,9 +115,91 @@ def getListing(sender, title2, url, media):
 			
 			
 	# for Audio Courses
-#	elif media == "audio":
-
-		
+	elif media == "audio":
+		url = url.replace("CourseHome/index.htm", "AudioLectures/index.htm")
+		page = XML.ElementFromURL(url, isHTML=True, errors="ignore")
+		if page != None:
+			audio = page.xpath("//div/table/tbody/tr/td[5]/a[1]/@href")
+			titles = page.xpath("//div/table/tbody/tr/td[5]/a[1]/parent::*/parent::*/td[2]")
+			if audio != []:
+				for x in range(len(audio)):
+					dir.Append(TrackItem(audio[x], title=titles[x].text,))
+			elif audio ==[]:
+				audio = page.xpath("//div/table/tbody/tr/td[3]/a/@href")
+				titles = page.xpath("//div/table/tbody/tr/td[3]/a/@href/parent::*/parent::*/parent::*/td[2]")
+				if audio != []:
+					for x in range(len(titles)):
+						dir.Append(TrackItem(audio[x], title=titles[x].text,))
+				elif audio == []:
+					audio = page.xpath("//div/table/tbody/tr/td[3]/a")
+					titles = page.xpath("//div/table/tbody/tr/td[3]/a/parent::*/parent::*/td[2]")
+					if audio != []:
+						for x in range(len(audio)):
+							dir.Append(TrackItem(audio[x], titles[x].replace("\n,",", ")))
+					elif audio ==[]:
+						audio = page.xpath("//div/table/tbody/tr/td[4]/a/@href")
+						titles = page.xpath("//div/table/tbody/tr/td[4]/a/parent::*//parent::*//td[2]")
+						for x in range(len(audio)):
+							dir.Append(TrackItem(audio[x], title=titles[x].text))
+						
+						
+		elif page == None:
+			url = url.replace("AudioLectures/index.htm", "LectureNotes/index.htm")
+			page = XML.ElementFromURL(url, isHTML=True, errors="ignore")
+			if page != None:
+				audio = page.xpath("//div/table/tbody/tr/td[3]/a/@href")
+				if audio != []:
+					titles = page.xpath("//div/table/tbody/tr/td[3]/a/parent::*/parent::*/td[2]/text()[1]")
+					for x in range(len(audio)):
+						dir.Append(TrackItem(audio[x], titles[x].replace("(","")))
+				
+				elif audio == []:
+					audio = page.xpath("//tr/td/span/a[2]")
+					if audio != []:
+						audio2 = page.xpath("//tr/td/span/a[3]")
+						titles = page.xpath("//tr/td/span/a[2]/parent::*/text()[1]")
+						for x in range(len(audio)):
+							dir.Append(TrackItem(audio[x], title=titles[x].replace("(","- part 1")))
+					
+					
+					
+					elif audio == []:
+						aURL = page.xpath("//div/table/tbody/tr/td[2]/a/@href")
+						titles = page.xpath("//div/table/tbody/tr/td[2]/a")
+						for x in range(len(aURL)):
+							aPage = XML.ElementFromURL("http://ocw.mit.edu"+aURL[x], isHTML=True, errors="ignore")
+							audio = str(aPage.xpath("//div[1]/table[2]/tbody/tr[1]/td[1]/p[2]/a[2]/@href")).strip("[]'")
+							if audio != "":
+								dir.Append(TrackItem(audio, title=titles[x].text))
+								
+			
+			elif page == None:
+				url = url.replace("LectureNotes/index.htm", "VideoLectures/index.htm")
+				page = XML.ElementFromURL(url, isHTML=True, errors="ignore")
+				if page != None:
+					audio = page.xpath("//div/table/tbody/tr/td[4]/a/@href")
+					titles = page.xpath("//div/table/tbody/tr/td[4]/a/parent::*/parent::*/td[2]")
+					if audio != []:
+						for x in range(len(audio)):
+							dir.Append(TrackItem(audio[x], titles[x].text))
+							
+						
+					elif audio == []:
+						aURL = page.xpath("//div/table/tbody/tr/td/a/@href")
+						titles = page.xpath("//div/table/tbody/tr/td/a")
+						for x in range(len(aURL)):
+							aPage = XML.ElementFromURL("http://ocw.mit.edu"+aURL[x], isHTML=True, errors="ignore")
+							audio = str(aPage.xpath("id('BodyCopy1')/div[1]/table[2]/tbody/tr[1]/td[2]/p[2]/a[2]/@href")).strip("[]'")
+							if audio != "":
+								dir.Append(TrackItem(audio, title=titles[x].text))
+							elif audio == "":
+								audio = str(aPage.xpath("//table[2]/tbody/tr[1]/td[2]/p[3]/a[2]/@href")).strip("[]'")
+								if audio != "":
+									dir.Append(TrackItem(audio, title=titles[x].text))
+								elif audio == "":
+									audio = str(aPage.xpath("//table[2]/tbody/tr[1]/td[2]/p[2]/a[2]/@href")).strip("[]'")
+									if audio != "":
+										dir.Append(TrackItem(audio, title=titles[x].text))	
 		
 	return dir
 
